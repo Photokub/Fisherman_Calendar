bcec21a3a8ff8bfe6382a1da8af9f71b4e065211
A_Kub_work <photokub@yandex.ru>
2023-08-15 19:21:17 +0300

    Feat: 3D-свойства DateElement и Carousel

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,8 +1,14 @@
 import React from "react";
 import {DateElement} from "../DateElement/DateElement";
+import './Carousel.css'
+
 
 export const Carousel:React.FC = () => {
     return(
-        <DateElement/>
+        <section className='carousel'>
+            <DateElement/>
+            <DateElement/>
+            <DateElement/>
+        </section>
     )
 }
--- src/components/DateElement/DateElement.tsx
+++ src/components/DateElement/DateElement.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import './DateElement.css';
 
-export const DateElement:React.FC = () => {
+export const DateElement: React.FC = () => {
     return (
         <section className='date-element'>
-
+            <h1 className='date-element__number'>24</h1>
+            <h2 className='date-element__month'>Декабря</h2>
         </section>
     )
 }
