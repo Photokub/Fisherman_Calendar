de4bd013c945faad48763de00942d2f83a134822
A_Kub_work <photokub@yandex.ru>
2023-08-15 16:43:27 +0300

    Feat: подключение стилей и шрифтов

---
+++ src/components/Carousel/Carousel.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import {DateElement} from "../DateElement/DateElement";
+
+export const Carousel:React.FC = () => {
+    return(
+        <DateElement/>
+    )
+}
---
+++ src/components/DateElement/DateElement.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import './DateElement.css';
+
+export const DateElement:React.FC = () => {
+    return (
+        <section className='date-element'>
+
+        </section>
+    )
+}
