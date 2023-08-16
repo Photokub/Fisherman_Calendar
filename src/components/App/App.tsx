a85a925bdeae7a94b7bcd776e5536d6d99ff2590
A_Kub_work <photokub@yandex.ru>
2023-08-16 19:34:33 +0300

    Feat: функции кнопок вперед-назад

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Carousel} from "../Carousel/Carousel";
 
 function App() {
+
+    const [activeCard, setActiveCard] = useState(true)
+    const [yesterdayCard, setYesterdayCard] = useState({day:'1', month: 'D'})
+    const [todayCard, setTodayCard] = useState({day:'2', month: 'D'})
+    const [tomorrowCard, setTomorrowCard] = useState({day:'3', month: 'D'})
+
+    function clickFroward() {
+        setTodayCard(yesterdayCard)
+        setTomorrowCard(todayCard)
+        //в yesterdayCard пойдёт ближайший сосед массива слева
+    }
+
+    function clickBack() {
+        setTodayCard(tomorrowCard)
+        setYesterdayCard(todayCard)
+        //в tomorrowCard пойдёт ближайший сосед массива справа
+    }
+
     return (
         <Carousel/>
     );
