7eb5c9ca5ab6da70428bec38f2e8300cbe92184e
A_Kub_work <photokub@yandex.ru>
2023-08-30 18:28:45 +0300

    Feat: получение данных location и astro с сервера

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -3,19 +3,42 @@ import './App.css';
 import {Carousel} from "../Carousel/Carousel";
 import data from '../data/data.json'
 import {SlideContext} from '../../context/SlideContext'
+import {api} from "../../api/Api";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
 function App() {
 
+    const [astroData, setAstroData] = useState({})
+    const [locationData, setLocationData] = useState({})
     const [daysArray, setDaysArray] = useState<[]>([])
     const [selectedDay, setSelectedDay] = useState(0)
 
-    useEffect(()=>{
+    useEffect(() => {
+        try {
+            const fetchData = async () => {
+                const data = await api.getAstroData()
+                if(!data){
+                    throw new Error('Не удалось получить данные')
+                }
+                setAstroData(data.astronomy.astro)
+                setLocationData(data.location)
+            }
+            fetchData()
+        } catch (err) {
+            console.log(`Ошибка ${err}`)
+        }
+    }, [])
+
+    useEffect(() => {
         const dataArray = data as []
         setDaysArray(dataArray)
         console.log(selectedDay)
     }, [])
 
     console.log(daysArray)
+    console.log(astroData)
+    console.log(locationData)
 
     const clickForward = () => {
         setSelectedDay(selectedDay + 1)
@@ -36,7 +59,7 @@ function App() {
             daysArray,
             selectedDay,
         }}>
-        <Carousel/>
+            <Carousel/>
         </SlideContext.Provider>
     );
 }
